ccf0723b91b87ea6abf685a21c78f6824c537586
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-26 18:59:23 -0600

    Add multiboot_header

---
+++ src/multiboot_header.asm
@@ -0,0 +1,11 @@
+section .multiboot_header 
+header_start:
+  dd 0xE85250D6
+  dd 0
+  dd header_end - header_start
+  dd -(0xE85250D6 + 0 + header_end - header_start)
+
+  dw 0
+  dw 0
+  dw 8
+header_end:
