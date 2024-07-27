99527b993865b76d359b9dd686af7c3ab12ef13c
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-26 19:05:22 -0600

    Annotate multiboot_header

--- src/multiboot_header.asm
+++ src/multiboot_header.asm
@@ -1,11 +1,13 @@
 section .multiboot_header 
 header_start:
-  dd 0xE85250D6
-  dd 0
-  dd header_end - header_start
-  dd -(0xE85250D6 + 0 + header_end - header_start)
+  dd 0xE85250D6                 ; magic number (u32)
+  dd 0                          ; architecture (u32)
+  dd header_end - header_start  ; header length (u32)
+  ; checksum (u32)
+  dd 0x100000000 - (0xE85250D6 + 0 + header_end - header_start)
 
-  dw 0
-  dw 0
-  dw 8
+  ; end tag (type, flags, size)
+  dw 0      ; endtag (u16)
+  dw 0      ; endtag (u16)
+  dw 8      ; endtag (u32)
 header_end:
